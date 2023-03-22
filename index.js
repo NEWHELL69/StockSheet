78135b922e489b9076052e849db9e17533d3852e
NEWHELL69 <kushagra0304@gmail.com>
2023-03-22 19:00:25 +0530

    setup_22-03-23

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+var morgan = require('morgan')
 
+const app = express();
+
+// If you change the directory, which serves the frontend, below then reloading the running
+// application would respond with 404 error and won't reflect the change until aplication is restarted.
 app.use(express.static('build'))
 
-// app.get("/", (req, res) => res.type('html').send(html));
+// ------------------------------------------------------------
+// The code below logs any request made to the server
 
+app.use(morgan(function (tokens, req, res) {
+  if(tokens.method(req, res) === "POST") {
+      return [
+          tokens.method(req, res),
+          tokens.url(req, res),
+          tokens.status(req, res),
+          tokens.res(req, res, 'content-length'), '-',
+          tokens['response-time'](req, res), 'ms',
+          JSON.stringify(req.body)
+      ].join(' ');
+  }
+
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+}))
+// ------------------------------------------------------------
+
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
-// const html = `
-// <!DOCTYPE html>
-// <html>
-//   <head>
-//     <title>Hello from Render!</title>
-//   </head>
-//   <body>
-//     <section>
-//       Hello from Render!
-//     </section>
-//   </body>
-// </html>
-// `

802add73997f0da3d6de870503605279eb919dcd
NEWHELL69 <kushagra0304@gmail.com>
2023-04-03 23:53:39 +0530

    added eslint

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-// There is no hot reloading. If you modify the code while the application is running then the changes 
+// There is no hot reloading.
+// If you modify the code while the application is running then the changes
 // will not be reflected
 
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require("express");
-const morgan = require('morgan')
-const mongoose = require('mongoose')
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // mongoose models
-const Reel = require('./models/reel')
-
+const Reel = require('./models/reel');
 
 const app = express();
 
 // ------------------------------------------------------------
 // Middlewares
 
-// The middleware below serves the frontend to the client. Basically it serves the index.js file in the 
+// The middleware below serves the frontend to the client.
+// Basically it serves the index.js file in the
 // build folder and executes it
-app.use(express.static('build'))
+app.use(express.static('build'));
 
 // This middleware converts the data recieved to the server in json format
-app.use(express.json())
+app.use(express.json());
 
 // This one logs any request made to the server
 // One important thing to note. it only logs those request which have a response.
 // No response indicates server error.
-app.use(morgan(function (tokens, req, res) {
-  if(tokens.method(req, res) === "POST") {
-      return [
-          tokens.method(req, res),
-          tokens.url(req, res),
-          tokens.status(req, res),
-          tokens.res(req, res, 'content-length'), '-',
-          tokens['response-time'](req, res), 'ms',
-          JSON.stringify(req.body)
-      ].join(' ');
+app.use(morgan((tokens, req, res) => {
+  if (tokens.method(req, res) === 'POST') {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body),
+    ].join(' ');
   }
 
   return [
@@ -43,141 +44,131 @@ app.use(morgan(function (tokens, req, res) {
     tokens.url(req, res),
     tokens.status(req, res),
     tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms'
-  ].join(' ')
-}))
+    tokens['response-time'](req, res), 'ms',
+  ].join(' ');
+}));
 
-// This middleware responds every request with 503 server error code if connection to database is not established
+// This middleware responds every request with 503 server error code
+// if connection to database is not established
 const handleDataBaseConnection = (request, response, next) => {
-  if(mongoose.connection.readyState !== 1) {
+  if (mongoose.connection.readyState !== 1) {
     response.status(503).json({
-        error: "Database not connected"
-    })
+      error: 'Database not connected',
+    });
   } else {
-    // It's important next() is called inside this else block. Because we need to end the request-response cycle.
-    next()
+    // It's important next() is called inside this else block.
+    // Because we need to end the request-response cycle.
+    next();
   }
-}
+};
 
-app.use(handleDataBaseConnection)
+app.use(handleDataBaseConnection);
 // ------------------------------------------------------------
 
 // ------------------------------------------------------------
 // mongoDB code below
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log("Connecting to MongoDB");
+console.log('Connecting to MongoDB');
 
-mongoose.connect(url).then((_) => {
-    console.log("Connection successfull")
+mongoose.connect(url).then(() => {
+  console.log('Connection successfull');
 }).catch((e) => {
-    console.log("Error connecting to MongoDB:", e.message)
+  console.log('Error connecting to MongoDB:', e.message);
 });
 
 // ------------------------------------------------------------
 
-
 // ------------------------------------------------------------
 // API
 
-//Reel API
+// Reel API
 
 const responseObj = (id, code, message, reel) => {
-  console.log(message)
+  console.log(message);
 
-  if(reel) {
+  if (reel) {
     // JSON.stringify calls the toJSON method defined on the document and does some other imp stuff.
-    reel = JSON.stringify(reel)
-    reel = JSON.parse(reel)
+    reel = JSON.stringify(reel);
+    reel = JSON.parse(reel);
 
     return {
-      id: id,
+      id,
       state: {
-        code: code,
-        message: message,
+        code,
+        message,
       },
       reel: {
-        ...reel
-      }
-    }
-  }else {
-    return {
-      id: id,
-      state: {
-        code: code,
-        message: message,
+        ...reel,
       },
-      reel: null
-    }
+    };
   }
-}
+  return {
+    id,
+    state: {
+      code,
+      message,
+    },
+    reel: null,
+  };
+};
 
 app.get('/api/reel/:id', (request, response) => {
-  let id = request.params.id;
+  const { id } = request.params;
 
   // In context of get request, the state codes have following meaning:
   // 1 -> Reel was found (intended behaviour)
   // 2 -> Reel was not found in database (inteded behaviour)
   // 0 -> server error
-
 
   Reel.findById(id)
-    .then(reel => {
-      if(reel) {
-
-        response.json(responseObj(id, 1, "Reel was found", reel))
-
+    .then((reel) => {
+      if (reel) {
+        response.json(responseObj(id, 1, 'Reel was found', reel));
       } else {
-
-        response.status(404).send(responseObj(id, 2, "Reel was not found", null));
-
+        response.status(404).send(responseObj(id, 2, 'Reel was not found', null));
       }
     }).catch((e) => {
-      console.log(e)
+      console.log(e);
 
-      response.status(400).send(responseObj(id, 0, e.message, null))
-    })
-})
+      response.status(400).send(responseObj(id, 0, e.message, null));
+    });
+});
 
 app.get('/api/reels', async (request, response) => {
-  const ids = request.query.ids;
-  const idArray = ids.split(",");
-  const reels = []
+  const { ids } = request.query;
+  const idArray = ids.split(',');
+  const reels = [];
 
   // In context of get request, the state codes have following meaning:
   // 1 -> Reel was found (intended behaviour)
   // 2 -> Reel was not found in database (inteded behaviour)
   // 0 -> server error
 
-  let id = "";
+  let id = '';
 
-  for(id of idArray) {
+  for (id of idArray) {
     try {
-      const reel = await Reel.findById(id)
+      const reel = await Reel.findById(id);
 
-      if(reel) {
-
-        reels.push(responseObj(id, 1, "Reel was found", reel))
-
+      if (reel) {
+        reels.push(responseObj(id, 1, 'Reel was found', reel));
       } else {
-
-        reels.push(responseObj(id, 2, "Reel was not found", null));
-        
+        reels.push(responseObj(id, 2, 'Reel was not found', null));
       }
-    } catch(e) {
-      console.log(e)
+    } catch (e) {
+      console.log(e);
 
-      reels.push(responseObj(id, 0, e.message, null))
+      reels.push(responseObj(id, 0, e.message, null));
     }
   }
 
-  response.json(reels)
-})
+  response.json(reels);
+});
 
 app.post('/api/reel', (request, response) => {
-
-  const body = request.body
+  const { body } = request;
 
   const reel = new Reel({
     gsm: body.gsm,
@@ -188,42 +179,38 @@ app.post('/api/reel', (request, response) => {
     bf: body.bf,
     sold: body.sold,
     soldTo: body.soldTo,
-    soldDate: body.soldDate
-  })
+    soldDate: body.soldDate,
+  });
 
   reel.save()
     .then((newReel) => {
-      // Unlike findById method which can resolve with null value, save method only resolves with the saved document, 
+      // Unlike findById method which can resolve with null value,
+      // save method only resolves with the saved document,
       // when the document is actually saved in database
-      if(newReel === reel){
-
-        response.json(responseObj(newReel.id, 1, "Reel is saved in database", newReel))
-
+      if (newReel === reel) {
+        response.json(responseObj(newReel.id, 1, 'Reel is saved in database', newReel));
       } else {
-
-        response.json(responseObj(null, 2, "Reel was not saved in database", null))
-
+        response.json(responseObj(null, 2, 'Reel was not saved in database', null));
       }
     }).catch((e) => {
-      console.log(e)
+      console.log(e);
 
-      response.status(400).json(responseObj(null, 0, e.message, null))
-    })
-})
+      response.status(400).json(responseObj(null, 0, e.message, null));
+    });
+});
 
 app.post('/api/reels', async (request, response) => {
   const reels = request.body;
-  const acknowledgments = []
+  const acknowledgments = [];
 
   // In context of post request, the state codes have following meaning:
   // 1 -> Reel was saved (intended behaviour)
   // 2 -> Reel was not saved (inteded behaviour)
   // 0 -> server error
 
-  let reel = {}
+  let reel = {};
 
-  for(reel of reels) {
-
+  for (reel of reels) {
     try {
       reel = new Reel({
         gsm: reel.gsm,
@@ -234,93 +221,76 @@ app.post('/api/reels', async (request, response) => {
         bf: reel.bf,
         sold: reel.sold,
         soldTo: reel.soldTo,
-        soldDate: reel.soldDate
-      })
+        soldDate: reel.soldDate,
+      });
 
-      const newReel = await reel.save(reel)
+      const newReel = await reel.save(reel);
 
-      if(newReel === reel) {
-
-        acknowledgments.push(responseObj(newReel.id, 1, "Reel was saved", reel))
-
+      if (newReel === reel) {
+        acknowledgments.push(responseObj(newReel.id, 1, 'Reel was saved', reel));
       } else {
-
-        acknowledgments.push(responseObj(null, 2, "Reel was not saved in database", null));
-        
+        acknowledgments.push(responseObj(null, 2, 'Reel was not saved in database', null));
       }
-    } catch(e) {
-      console.log(e)
+    } catch (e) {
+      console.log(e);
 
-      acknowledgments.push(responseObj(null, 0, e.message, null))
+      acknowledgments.push(responseObj(null, 0, e.message, null));
     }
   }
 
-  response.json(acknowledgments)
-})
+  response.json(acknowledgments);
+});
 
 app.delete('/api/reel/:id', (request, response) => {
-
   // This conversion here is necessary
-  const id = new mongoose.Types.ObjectId(request.params.id)
+  const id = new mongoose.Types.ObjectId(request.params.id);
 
   Reel.deleteOne(id).then((res) => {
-      if(res.deletedCount === 1) {
-
-          response.json(responseObj(id, 1, "Reel was deleted from database", null))
-
-      } else if(res.deletedCount === 0) {
-
-        response.json(responseObj(id, 2, "Reel was not found and hence not deleted", null))
-
-      }
+    if (res.deletedCount === 1) {
+      response.json(responseObj(id, 1, 'Reel was deleted from database', null));
+    } else if (res.deletedCount === 0) {
+      response.json(responseObj(id, 2, 'Reel was not found and hence not deleted', null));
+    }
   }).catch((e) => {
-
-    response.json(responseObj(id, 0, e.message, null))
-
-  })
-})
+    response.json(responseObj(id, 0, e.message, null));
+  });
+});
 
 app.delete('/api/reels', async (request, response) => {
-
-  const ids = request.query.ids;
-  const idArray = ids.split(",");
-  const acknowledgments = []
+  const { ids } = request.query;
+  const idArray = ids.split(',');
+  const acknowledgments = [];
 
   // In context of delete request for reel, the state codes have following meaning:
   // 1 -> Reel deletion successfull (intended behaviour)
   // 2 -> Reel was not found in database (inteded behaviour)
   // 0 -> server error
 
-  for(id of idArray) {
+  for (id of idArray) {
     try {
       // This conversion here is necessary
-      id = new mongoose.Types.ObjectId(id)
+      id = new mongoose.Types.ObjectId(id);
 
-      const res = await Reel.deleteOne(id)
+      const res = await Reel.deleteOne(id);
 
-      if(res.deletedCount === 1) {
-
-        acknowledgments.push(responseObj(id, 1, "Reel was deleted", null))
-
-      } else if(res.deletedCount === 0){
-
-        acknowledgments.push(responseObj(id, 2, "Reel was not found and hence not deleted", null));
-
+      if (res.deletedCount === 1) {
+        acknowledgments.push(responseObj(id, 1, 'Reel was deleted', null));
+      } else if (res.deletedCount === 0) {
+        acknowledgments.push(responseObj(id, 2, 'Reel was not found and hence not deleted', null));
       }
-    } catch(error) {
-        console.log(error);
+    } catch (error) {
+      console.log(error);
 
-        acknowledgments.push(responseObj(id, 0, error.message, null))
-      }
+      acknowledgments.push(responseObj(id, 0, error.message, null));
+    }
   }
 
-  response.json(acknowledgments)
-})
+  response.json(acknowledgments);
+});
 
 app.put('/api/reel/:id', (request, response) => {
-
-  const body = request.body;
-  const id = request.params.id
+  const { body } = request;
+  const { id } = request.params;
 
   const updationToReel = {
     gsm: body.gsm,
@@ -331,20 +301,15 @@ app.put('/api/reel/:id', (request, response) => {
     bf: body.bf,
     sold: body.sold,
     soldTo: body.soldTo,
-    soldDate: body.soldDate
-  }
+    soldDate: body.soldDate,
+  };
 
-  Reel.findByIdAndUpdate(id, updationToReel, {new: true}).then((newReel) => {
-
-    response.json(responseObj(newReel.id, 1, "Document was found and updated", newReel))
-
+  Reel.findByIdAndUpdate(id, updationToReel, { new: true }).then((newReel) => {
+    response.json(responseObj(newReel.id, 1, 'Document was found and updated', newReel));
   }).catch((e) => {
-
-    response.status(400).send(responseObj(id, 0, e.message, null))
-
-  })
-
-})
+    response.status(400).send(responseObj(id, 0, e.message, null));
+  });
+});
 
 // ------------------------------------------------------------
 
@@ -352,17 +317,17 @@ app.put('/api/reel/:id', (request, response) => {
 // Error handling for reel api
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } 
+    return response.status(400).send({ error: 'malformatted id' });
+  }
 
-  next(error)
-}
+  next(error);
+};
 
 // this has to be the last loaded middleware.
-app.use(errorHandler)
+app.use(errorHandler);
 
 // ------------------------------------------------------------
 
--- models/reel.js
+++ models/reel.js
@@ -1,27 +1,28 @@
-const mongoose = require('mongoose')
+/* eslint-disable no-param-reassign */
+const mongoose = require('mongoose');
 
 // Reel id is created by mongoose when we save a document
 const ReelSchema = new mongoose.Schema({
-    gsm: Number,
-    size: Number,
-    date: { type: Date, default: Date.now },
-    shipment: String,
-    shade: String,
-    annotations: String,
-    bf: Number,
-    sold: Boolean,
-    soldTo: String,
-    soldDate: { type: Date, default: null }
-})
+  gsm: Number,
+  size: Number,
+  date: { type: Date, default: Date.now },
+  shipment: String,
+  shade: String,
+  annotations: String,
+  bf: Number,
+  sold: Boolean,
+  soldTo: { type: String, default: null },
+  soldDate: { type: Date, default: null },
+});
 
 ReelSchema.set('toJSON', {
-    transform: (document, returnedObject, options) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
 // The first argument to the function below dictates the collection to put new documents in.
 // This is fragile
-module.exports = mongoose.model('Reel', ReelSchema)
+module.exports = mongoose.model('Reel', ReelSchema);

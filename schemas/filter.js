90b8d38db029ef03d7fd3c77061e2018e85125aa
NEWHELL69 <kushagra0304@gmail.com>
2023-04-10 13:41:26 +0530

    added filterSchema

--- schemas/filter.js
+++ schemas/filter.js
@@ -18,7 +18,7 @@ const filterSchema = new mongoose.Schema({
 filterSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
+    delete returnedObject._id; 
     delete returnedObject.__v;
   },
 });
